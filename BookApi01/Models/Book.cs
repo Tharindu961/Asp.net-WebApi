8405e12297f523ff95554c81b5a8afbdf1c44cb9
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2021-11-18 21:11:21 +0530

    Added context model

--- BookApi01/Models/Book.cs
+++ BookApi01/Models/Book.cs
@@ -7,5 +7,9 @@ namespace BookApi01.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
     }
 }
