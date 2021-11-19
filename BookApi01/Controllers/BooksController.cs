f91c5285f8b7d872ebf082770f145cc0ae6b88f8
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2021-11-19 08:28:38 +0530

    Added Controller class

---
+++ BookApi01/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using BookApi01.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+    }
+}
